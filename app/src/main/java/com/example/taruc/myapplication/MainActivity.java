def7b7e76a95f43b4c711a1475c9419bed1c5934
weiguan963 <limwg-wa16@student.tarc.edu.my>
2018-10-16 10:26:57 +0800

    testing

---
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.taruc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity Tang","Log message");
+    }
+}
